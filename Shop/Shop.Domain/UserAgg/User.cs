5dad9aa83ce48fb27bb1279cd73b77e40534aa3c
hoseein <mohammadmesri13@gmail.com>
2022-04-08 15:42:54 +0430

    add user agg

---
+++ Shop/Shop.Domain/UserAgg/User.cs
@@ -0,0 +1,98 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Shop.Domain.UserAgg.Enums;
+using Shop.Domain.UserAgg.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.UserAgg
+{
+    public class User : AggregateRoot
+    {
+        public User(string name, string family, string email, string phonenumber, string password, Gender gender,
+            IDomainUserService domainservice)
+        {
+            Guard(email, phonenumber, domainservice);
+            Name = name;
+            Family = family;
+            Email = email;
+            Phonenumber = phonenumber;
+            Password = password;
+            Gender = gender;
+        }
+
+        public string Name { get; private set; }
+        public string Family { get; private set; }
+        public string Email { get; private set; }
+        public string Phonenumber { get; private set; }
+        public string Password { get; private set; }
+        public Gender Gender { get; private set; }
+        public List<UserRole> Roles { get; private set; }
+        public List<Wallet> Wallets { get; private set; }
+        public List<UserAddress> Addresses { get; private set; }
+
+
+
+        public void Edit(string name, string family, string email, string phonenumber, Gender gender,IDomainUserService domainservice)
+        {
+            Guard(email, phonenumber,domainservice);
+            Name = name;
+            Family = family;
+            Email = email;
+            Phonenumber = phonenumber;
+            Gender = gender;
+        }
+        public static User Register(string email,string phonenumber,string password, IDomainUserService domainservice)
+        {
+            return new User("", "", email,phonenumber, password, Gender.None, domainservice);
+        }
+        public void AddAddress(UserAddress address)
+        {
+            address.UserId = Id;
+            Addresses.Add(address);
+        }
+        public void EditAddress(UserAddress address)
+        {
+            var oldadress = Addresses.FirstOrDefault(f => f.Id == address.Id);
+            if (oldadress == null)
+                throw new NullOrEmptyDomainDataException("Address not found");
+            Addresses.Remove(oldadress);
+            Addresses.Add(address);
+        }
+        public void DeleteAddress(long addresid)
+        {
+            var oldadress = Addresses.FirstOrDefault(f => f.Id == addresid);
+            if (oldadress == null)
+                throw new NullOrEmptyDomainDataException("Address not found");
+            Addresses.Remove(oldadress);
+        }
+        public void ChargeWallet(Wallet wallet)
+        {
+            Wallets.Add(wallet);
+        }
+        public void SetRoles(List<UserRole> roles)
+        {
+            roles.ForEach(f => f.UserId = Id);
+            Roles.Clear();
+            Roles.AddRange(roles);
+        }
+        public void Guard(string email, string phonenumber,IDomainUserService domainservice)
+        {
+            NullOrEmptyDomainDataException.CheckString(phonenumber, nameof(phonenumber));
+            NullOrEmptyDomainDataException.CheckString(email, nameof(email));
+            if (phonenumber.Length != 11)
+                throw new InvalidDomainDataException("شماره وارد شده معتبر نمیباشد");
+            if (email.IsValidEmail() == false)
+                throw new InvalidDomainDataException("ایمیل وارد شده نا معتبر است");
+            if (phonenumber != Phonenumber)
+                if (domainservice.IsPhonenumberExist(phonenumber))
+                    throw new InvalidDomainDataException("شماره موبایل تکراری است");
+            if (email != Email)
+                if (domainservice.IsEmailExist(email))
+                    throw new InvalidDomainDataException("ایمیل تکراری است");
+        }
+    }
+}
---
+++ Shop/Shop.Domain/UserAgg/UserAddress.cs
@@ -0,0 +1,64 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+
+namespace Shop.Domain.UserAgg
+{
+    public class UserAddress : BaseEntity
+    {
+        public UserAddress(string shire, string city, string postalCode, string postalAddress, string phonenumber, string name, string family, string nationalCode)
+        {
+            Guard(shire, city, postalCode, postalAddress, phonenumber, name, family, nationalCode);
+            Shire = shire;
+            City = city;
+            PostalCode = postalCode;
+            PostalAddress = postalAddress;
+            Phonenumber = phonenumber;
+            Name = name;
+            Family = family;
+            NationalCode = nationalCode;
+        }
+
+        public long UserId { get; internal set; }
+        public string Shire { get; private set; }
+        public string City { get; private set; }
+        public string PostalCode { get; private set; }
+        public string PostalAddress { get; private set; }
+        public string Phonenumber { get; private set; }
+        public string Name { get; private set; }
+        public string Family { get; private set; }
+        public string NationalCode { get; private set; }
+        public bool ActiveAdress { get; private set; }
+
+        public void Edit(string shire, string city, string postalCode, string postalAddress, string phonenumber, string name, string family, string nationalCode)
+        {
+            Guard(shire,city,postalCode,postalAddress,phonenumber,name,family,nationalCode);
+            Shire = shire;
+            City = city;
+            PostalCode = postalCode;
+            PostalAddress = postalAddress;
+            Phonenumber = phonenumber;
+            Name = name;
+            Family = family;
+            NationalCode = nationalCode;
+            ActiveAdress = false;
+        }
+        public void SetActive()
+        {
+            ActiveAdress = true;
+        }
+        public void Guard(string shire, string city, string postalCode, string postalAddress, string phonenumber, string name, string family, string nationalCod)
+        {
+            NullOrEmptyDomainDataException.CheckString(shire, nameof(shire));
+            NullOrEmptyDomainDataException.CheckString(city, nameof(city));
+            NullOrEmptyDomainDataException.CheckString(postalCode, nameof(postalCode));
+            NullOrEmptyDomainDataException.CheckString(postalAddress, nameof(postalAddress));
+            NullOrEmptyDomainDataException.CheckString(phonenumber, nameof(phonenumber));
+            NullOrEmptyDomainDataException.CheckString(name, nameof(name));
+            NullOrEmptyDomainDataException.CheckString(family, nameof(family));
+            NullOrEmptyDomainDataException.CheckString(nationalCod, nameof(nationalCod));
+
+            if (IranianNationalIdChecker.IsValid(nationalCod) == false)
+                throw new InvalidDomainDataException("شماره ملی نا معتبر است");
+        }
+    }
+}
