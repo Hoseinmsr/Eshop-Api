462c837073e62031a94e81b9935547cd41fe5f5c
hoseein <mohammadmesri13@gmail.com>
2022-04-19 18:22:43 +0430

    comment usecases

---
+++ Shop/Shop.Aplication/Comments/ChangeStatus/ChangeStatusCommentCommand.cs
@@ -0,0 +1,14 @@
+﻿using Common.Application;
+using Common.Application.Validation;
+using FluentValidation;
+using Shop.Domain.CommentAgg.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Aplication.Comments.ChangeStatus
+{
+    public record ChangeStatusCommentCommand(long Id,CommentStatus Status):IBaseCommand;
+}
---
+++ Shop/Shop.Aplication/Comments/Create/CreateCommentCommand.cs
@@ -0,0 +1,11 @@
+﻿using Common.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Aplication.Comments.Create
+{
+    public record CreateCommentCommand(long UserId,long ProductId,string Text):IBaseCommand;
+}
